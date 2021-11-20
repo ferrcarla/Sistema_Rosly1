1e4681d93a6b849e765bd8f9cbfc0546dcb0bc6d
GatiGrillo <graceheyrockym@gmail.com>
2021-11-20 15:50:32 -0400

    aumentando

--- admin/clientes/index.php
+++ admin/clientes/index.php
@@ -60,6 +60,23 @@
             </div>
           </div>
         </nav>
+
+
+        <div id="content">
+          <section>
+            <div class="container">
+              <div class="row"> 
+                <div class="col-lg-9">
+                  <h1 class="font-weight-bold mb-0">Bienvenido Usuario</h1>
+                  <p class="lead text-muted">Revisa la ultima informacion</p>
+                </div>
+                <div clas="col-lg-3">
+                  <button class="btn btn-primary">Descargar Reporte</button> 
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>    
     </div>
     
