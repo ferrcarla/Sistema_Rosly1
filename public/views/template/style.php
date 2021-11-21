8641f18a1c0e6fe875f6f7bbab985c18c133ecdc
Elmer Coronel Lima <ecoronel@pragmainvest.com.bo>
2021-11-21 07:51:24 -0400

    agregando datatable

--- public/views/template/style.php
+++ public/views/template/style.php
@@ -1,5 +1,7 @@
 <!-- Bootstrap CSS -->
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo ROOT; ?>img/logos.png">
 <link rel="stylesheet" href="<?php echo ROOT; ?>resources/assets/css/bootstrap.min.css">
 <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo ROOT; ?>resources/assets/css/style.css">
+<link rel="stylesheet" href="<?php echo ROOT; ?>resources/assets/DataTables/datatables.min.css">
