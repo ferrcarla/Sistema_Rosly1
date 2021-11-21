176de5fc8a9fa87178bb8e19c53a05f4cdbec163
GatiGrillo <graceheyrockym@gmail.com>
2021-11-21 07:58:56 -0400

    arreglos

--- public/views/client/index.php
+++ public/views/client/index.php
@@ -1,3 +1,5 @@
-<div class="row">
-    aqui contenido
-</div>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Abrir modal</button>
+
+<?php
+    require_once('registro_modal.php');
+?>
