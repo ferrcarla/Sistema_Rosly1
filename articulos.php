76c042b12a94db1d04296971df4fe5bd4618da50
Elmer Coronel Lima <ecoronel@pragmainvest.com.bo>
2021-11-23 04:11:21 -0400

    para la publicidad

---
+++ articulos.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Rosly Alta Costura</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/x-icon" href="img/logos.png">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/LineIcons.2.0.css">
+		<link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/stile.css">
+    </head>
+    <body>
+        <!-- =========== header start ============== -->
+        <header class="header">
+            <div class="navbar-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <nav class="navbar navbar-expand-lg">
+                                <a class="navbar-brand" href="index.php"><img src="img/logos.png" alt="Logo" width="80"></a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="toggler-icon"></span>
+                                    <span class="toggler-icon"></span>
+                                    <span class="toggler-icon"></span>
+                                </button>
+
+                                <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                                    <ul id="nav" class="navbar-nav ml-auto">
+                                        <li class="nav-item"><a class="page-scroll active" href="#inicio">Inicio</a></li>
+                                        <li class="nav-item"><a class="page-scroll" href="#caracteristica">Tienda</a></li>
+                                        <li class="nav-item"><a class="page-scroll" href="#procesos">Mision y vision</a></li>                                        
+                                    </ul>
+                                </div> <!-- navbar collapse -->
+                            </nav> <!-- navbar -->
+                        </div>
+                    </div> <!-- row -->
+                </div> <!-- container -->
+            </div> <!-- navbar area -->
+
+        </header>
+        <!-- ======== header end ================ -->
+     
+
+        <!--========================= CARACTERISTICAS ========================= -->
+        <section id="caracteristica" class="service-section gray-bg pt-150 pb-70">
+            <div class="container">
+                <article class="row">
+                    <div class="col-xl-5 col-lg-6 col-md-8 mx-auto">
+                        <div class="section-title text-center mb-55">
+                            <span class="wow fadeInDown text-danger" data-wow-delay=".2s">Nuestra Actividad</span>
+                            <h2 class="mb-15 wow fadeInUp" data-wow-delay=".4s">Puntos de Venta </h2>
+                            <p class="wow fadeInUp" data-wow-delay=".6s">.</p>
+                        </div>
+                    </div>
+                </article>
+                <article class="row">
+                    <div class="col-xl-4 col-lg-4 col-md-6">
+                        <div class="hero-img">
+                            <img  class="wow fadeInRight img-fluid img-thumbnail" data-wow-delay=".4s" data-wow-duration="1.3s" src="img/de3.jpg" width="400" height="100" alt="">
+                            <div class="section-title text-center mb-55">
+                            <span class="wow fadeInDown text-danger" data-wow-delay=".2s">PRENDAS
+                             <p class="wow fadeInUp" data-wow-delay=".6s"></p>
+                            <a href="#caracteristica" class="theme-btn wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1.3s">Ver Mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6">
+                         <div class="hero-img">
+                            <img  class="wow fadeInRight img-fluid img-thumbnail" data-wow-delay=".4s" data-wow-duration="1.3s" src="img/de4.jpg" width="400" height="100" alt="">
+                            <div class="section-title text-center mb-55">
+                            <span class="wow fadeInDown text-danger" data-wow-delay=".2s">NOVEDADES
+                            <p class="wow fadeInUp" data-wow-delay=".6s"></p>
+                             <a href="public/publicidad.php" class="theme-btn wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1.3s">Ver Mas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6">
+                         <div class="hero-img">
+                            <img  class="wow fadeInRight img-fluid img-thumbnail" data-wow-delay=".4s" data-wow-duration="1.3s" src="img/de2.jpg" width="400" height="100" alt="">
+                            <div class="section-title text-center mb-55">
+                            <span class="wow fadeInDown text-danger" data-wow-delay=".2s">OFERTAS
+                            <p class="wow fadeInUp" data-wow-delay=".6s"></p>
+                             <a href="public/publicidad.php" class="theme-btn wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1.3s">Ver Mas</a>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </section>
+         <!--========= CARACTERISTICAS END========== -->
+        <!--=========TEAM =======-->
+        
+        <!--=========TEAM END=======-->
+        
+        <!--  ======= SUSCRIBETE end ========== -->
+
+        <!-- ========================= FOOTER start ========================= -->
+        <footer class="footer gray-bg">
+              <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="footer-links text-center">
+                            <br>
+                            <a href="index.php" class="logo"><img src="img/rosly.png" alt="" width="100"> </a>
+                            <div class="footer-social-links">
+                                <ul>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-instagram-original"></i></a></li>
+                                </ul>
+                            </div>
+                            <nav class="footer-menu">
+                                <ul>
+                                    <li><a href="#inicio">Inicio</a></li>
+                                    <li><a href="#caracteristica">Servicios</a></li>
+                                    <li><a href="#procesos">Procesos</a></li>
+                                    <li><a href="#team">Team</a></li>
+                                    <li><a href="#contact">Contactos</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright-area text-center">
+                    <p class="mb-0">Diseñado y desarrollado por: <a href="#" rel="nofollow" target="_blank">@Sonia Marlene Tinta Chambi</a></p>
+                </div>
+            </div>
+        </footer>
+        <!-- ========================= FOOTER end ========================= -->
+
+
+        <!-- ========================= scroll-top ========================= -->
+        <a href="#" class="scroll-top"><i class="lni lni-chevron-up"></i></a>
+
+		<!-- ========================= JS here ========================= -->
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+		<script src="js/count-up.min.js"></script>
+		<script src="js/wow.min.js"></script>
+		<script src="js/main.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
